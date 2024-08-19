ca30da6a330c7f217a158d94f83c9353f3d80137
42inshin <inshin@student.42seoul.kr>
2024-08-19 17:57:43 +0900

    FEAT: Create Account UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./routes/home";
 import Profile from "./routes/profile";
 import Login from "./routes/login";
 import CreateAccount from "./routes/create-account";
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import { useEffect, useState } from "react";
 import LoadingScreen from "./components/loading-screen";
@@ -47,6 +47,12 @@ const GlobalStyles = createGlobalStyle`
   }
 `;
 
+const Wrappper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
 function App() {
   const [isLoading, setLoading] = useState(true);
   const init = async () => {
@@ -58,10 +64,10 @@ function App() {
   }, []);
 
   return (
-    <>
+    <Wrappper>
       <GlobalStyles />
       {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
-    </>
+    </Wrappper>
   );
 }
 
